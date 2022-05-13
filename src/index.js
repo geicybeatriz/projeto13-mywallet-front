6678daeb235b32ee586acdac3fd18e84d1d7ba29
ElderGr <gbielrs@gmail.com>
2022-05-13 17:09:00 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import "./assets/css/reset.css";
-import App from './Components/App/App';
-
-const root = document.querySelector(".root");
-ReactDOM.render(<App/>, root);
